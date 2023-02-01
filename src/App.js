fe744d5897436c1b821b5badf67382393575c0a7
gyawali9 <roshangyawali9@gmail.com>
2023-02-01 22:28:24 +0545

    feat: header structure and styling

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
+import "./styles/App.scss";
+import "./styles/header.scss";
+
+import Header from "./components/Header";
+
 const App = () => {
   return (
     <Router>
+      <Header />
       <Routes>AppS</Routes>
     </Router>
   );
